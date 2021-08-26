c6e9ef60f743ac5fa260ea0e3f588dbe730ad46e
Korobulya <Korobitsinia@gmail.com>
2021-08-26 17:22:03 +0300

    i study)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import React from "react";
 import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
 import Drawer from "./components/Drawer/Drawer";
+import {logDOM} from "@testing-library/react";
+
+const arr = [
+    {name: "Мужские Кроссовки Nike Blazer Mid Suede", price: "12999"},
+    {name: "Мужские Кроссовки Nike Air Max 270", price: "12999"},
+    {name: "Мужские Кроссовки Nike Blazer Mid Suede", price: "8999"},
+    {name: "Кроссовки Puma X Aka Boku Future Rider", price: "8999"},
+    {name: "Мужские Кроссовки Under Armour Curry 8", price: "15199"},
+    {name: "Мужские Кроссовки Nike Kyrie 7", price: "11299"},
+    {name: "Мужские Кроссовки Jordan Air Jordan 11", price: "10799"},
+    {name: "Мужские Кроссовки Nike LeBron XVIII", price: "16499"},
+    {name: "Мужские Кроссовки Nike Lebron XVIII Low", price: "13999"},
+    {name: "Мужские Кроссовки Nike Blazer Mid Suede", price: "8499"},
+    {name: "Кроссовки Puma X Aka Boku Future Rider", price: "8499"},
+    {name: "Мужские Кроссовки Nike Kyrie Flytrap IV", price: "11299"},
+]
 
 function App() {
     return (
@@ -18,9 +34,12 @@ function App() {
                     </div>
                 </div>
                 <div className="d-flex flex-wrap justify-between sneakers">
-                    <Card/>
-                    <Card/>
-                    <Card/>
+                    {arr.map((sneakers, idx) => (
+                        <Card key={idx}
+                              title={sneakers.name}
+                              price={sneakers.price}
+                              id={idx}/>
+                    ))}
                 </div>
             </div>
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,22 @@
 import React from "react";
+import styles from './Card.module.scss'
 
-const Card = () => {
+console.log(styles)
+const Card = ({title, price, id}) => {
+
     return (
-        <div className="card">
-            <div className={"favorite"}>
+        <div className={styles.card}>
+            <div className={styles.favorite}>
                 <img src={"/img/heart-unlike.svg"} alt={"unliked"}/>
             </div>
-            <img width={133} height={112} src={"/img/sneakers/1.jpg"} alt={"sneakers"}/>
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <img width={133} height={112} src={`/img/sneakers/${id + 1}.jpg`} alt={"sneakers"}/>
+            <h5>{title}</h5>
             <div className={"d-flex justify-between align-center"}>
                 <div className={"d-flex flex-column"}>
                     <span>Цена</span>
-                    <b>99 999</b>
+                    <b>{price} руб.</b>
                 </div>
-                <button className={"button"}>
+                <button onClick={() => console.log(`${id} add`)} className={"button"}>
                     <img width={11} height={11} src="/img/plus.svg" alt={"Plus"}/>
                 </button>
             </div>

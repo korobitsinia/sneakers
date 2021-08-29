91f8cd14fc6192c73a5977e6fab04cfca4ab5670
Igor Korobitsini <korobitsinia@gmail.com>
2021-08-29 21:15:05 +0300

    5 1.10

--- src/App.js
+++ src/App.js
@@ -1,78 +1,77 @@
 import React, {useEffect} from "react";
+import axios from "axios";
 
 import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
 import Drawer from "./components/Drawer/Drawer";
-import {logDOM} from "@testing-library/react";
 
-const arr = [
-    {name: "Мужские Кроссовки Nike Blazer Mid Suede", price: "12999", imageUrl: "/img/sneakers/1.jpg"},
-    {name: "Мужские Кроссовки Nike Air Max 270", price: "12999", imageUrl: "/img/sneakers/2.jpg"},
-    {name: "Мужские Кроссовки Nike Blazer Mid Suede", price: "8999", imageUrl: "/img/sneakers/3.jpg"},
-    {name: "Кроссовки Puma X Aka Boku Future Rider", price: "8999", imageUrl: "/img/sneakers/4.jpg"},
-    {name: "Мужские Кроссовки Under Armour Curry 8", price: "15199", imageUrl: "/img/sneakers/5.jpg"},
-    {name: "Мужские Кроссовки Nike Kyrie 7", price: "11299", imageUrl: "/img/sneakers/6.jpg"},
-    {name: "Мужские Кроссовки Jordan Air Jordan 11", price: "10799", imageUrl: "/img/sneakers/7.jpg"},
-    {name: "Мужские Кроссовки Nike LeBron XVIII", price: "16499", imageUrl: "/img/sneakers/8.jpg"},
-    {name: "Мужские Кроссовки Nike Lebron XVIII Low", price: "13999", imageUrl: "/img/sneakers/9.jpg"},
-    {name: "Мужские Кроссовки Nike Blazer Mid Suede", price: "8499", imageUrl: "/img/sneakers/10.jpg"},
-    {name: "Кроссовки Puma X Aka Boku Future Rider", price: "8499", imageUrl: "/img/sneakers/11.jpg"},
-    {name: "Мужские Кроссовки Nike Kyrie Flytrap IV", price: "11299", imageUrl: "/img/sneakers/12.jpg"},
-]
 
 function App() {
-
     const [items, setItems] = React.useState([])
     const [cartItems, setCartItems] = React.useState([])
+    const [cartOpened, setCartOpened] = React.useState(false)
+    const [searchValue, setSearchValue] = React.useState("")
+
     useEffect(() => {
-            const fetchData = async () => {
-                const a = await fetch("https://6128df510e3482001777b1e6.mockapi.io/items")
-                const b = await a.json()
-                setItems(b)
-            }
-            fetchData()
+            axios.get("https://6128df510e3482001777b1e6.mockapi.io/items")
+                .then((res) => setItems(res.data))
+            axios.get("https://6128df510e3482001777b1e6.mockapi.io/cart")
+                .then((res) => setCartItems(res.data))
         }
         , [])
 
-    const [cartOpened, setCartOpened] = React.useState(false)
     const handleToggleOpenCart = () => {
         setCartOpened((v) => !v)
     }
 
     const onAddToCart = (obj) => {
-        let v = cartItems.find((e) => e.title == obj.title)
-        if (v) {
-            return
+        let thisItemInCart = cartItems.find((item) => item.title === obj.title)
+        if (!thisItemInCart) {
+            axios.post("https://6128df510e3482001777b1e6.mockapi.io/cart", obj);
+            setCartItems((value) => ([...value, obj]))
         }
+    }
 
+    const onDeleteFromCart = (id) => {
+        // const newArrToCart = cartItems.filter((item) => item.title !== title)
+        // setCartItems(() => newArrToCart)
+        // axios.delete(`https://6128df510e3482001777b1e6.mockapi.io/cart/${id}`);
 
-        setCartItems((value) => ([...value, obj]))
+        console.log(id)
+        setCartItems((prev) => prev.filter((i) => i.id !== id))
+    }
+
+    const searchHandler = (e) => {
+        setSearchValue(prev => e.target.value)
     }
 
     return (
         <div className="App clear">
-            {cartOpened && <Drawer onClose={handleToggleOpenCart} items={cartItems}/>}
+            {cartOpened &&
+            <Drawer onClose={handleToggleOpenCart} items={cartItems} onDeleteFromCart={onDeleteFromCart}/>}
             <Header
                 onClickCart={handleToggleOpenCart}/>
             <div className="p-40  content">
                 <div className={"d-flex align-center justify-between mb-40"}>
-                    <h1>Все кроссовки</h1>
+                    <h1> {searchValue ? `Поиск по запросу: ${searchValue}` : 'Все кроссовки'} </h1>
                     <div className={"search-block d-flex"}>
                         <img src={"/img/search.svg"} alt={"search"}/>
-                        <input placeholder={"Search... "}/>
+                        <input placeholder={"Search... "} onChange={searchHandler} value={searchValue}/>
                     </div>
                 </div>
                 <div className="d-flex flex-wrap justify-between sneakers">
-                    {items.map((sneakers, idx) => (
-                        <Card key={idx}
-                              title={sneakers.name}
-                              price={sneakers.price}
-                              imageUrl={sneakers.imageUrl}
-                              id={idx}
-                              onClickFavorite={() => console.log('add to favorite')}
-                              onClickPlus={(obj) => onAddToCart(obj)}
-                        />
-                    ))}
+                    {items
+                        .filter((i) => i.name.includes(searchValue))
+                        .map((sneakers, idx) => (
+                            <Card key={idx}
+                                  title={sneakers.name}
+                                  price={sneakers.price}
+                                  imageUrl={sneakers.imageUrl}
+                                  id={idx}
+                                  onClickFavorite={() => console.log('add to favorite')}
+                                  onClickPlus={(obj) => onAddToCart(obj)}
+                            />
+                        ))}
                 </div>
             </div>
         </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
     return (
         <header className={"d-flex justify-between align-center p-40 header"}>
             <div className={"d-flex align-center"}>
-                <img width={40} height={40} src="/img/logo.png"/>
+                <img width={40} height={40} src="/img/logo.png" alt={"LOGO"}/>
                 <div>
                     <h3 className={"text-uppercase"}>React sneakers</h3>
                     <p className={"opacity-5"}>Магазин лучших кроссовок</p>
@@ -13,10 +13,10 @@ const Header = (props) => {
 
             <ul className={"headerRight d-flex cu-p"}>
                 <li className={"mr-30"} onClick={props.onClickCart}>
-                    <img width={18} height={18} src="/img/cart.svg"/>
+                    <img width={18} height={18} src="/img/cart.svg" alt={"icon"}/>
                     <span>1205 rub</span></li>
                 <li>
-                    <img width={18} height={18} src="/img/user.svg"/>
+                    <img width={18} height={18} src="/img/user.svg" alt={"icon"}/>
                 </li>
             </ul>
 
